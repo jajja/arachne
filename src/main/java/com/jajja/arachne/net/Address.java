4703cf912d7ff9a200fc25f2ee378b0e6a269439
Martin Korinth <martin@korinth.se>
2013-04-13 23:17:14 +0200

    Added address parsing, now need sufficient test set

--- src/main/java/com/jajja/arachne/net/Address.java
+++ src/main/java/com/jajja/arachne/net/Address.java
@@ -31,8 +31,8 @@ import com.jajja.arachne.exceptions.MalformedAddress;
 public class Address extends Host {
     
     private String hex;
-    private boolean isIpv4;
-    private boolean isIpv6;
+    private String comment;
+    private int ipv;   
     
     /**
      * Creates an address by parsing the name.
@@ -47,8 +47,23 @@ public class Address extends Host {
         parse();
     }
     
+    /**
+     * Provides the hexadecimal representation parsed from the address.
+     * 
+     * @return the hexadecimal representation
+     */
     public String getHex() {
         return hex;
+    }
+    
+    
+    /**
+     * Provides any comment parsed from the address.
+     * 
+     * @return the comment of the address or null for no comment
+     */
+    public String getComment() {
+        return comment;
     }
     
     /**
@@ -57,7 +72,7 @@ public class Address extends Host {
      * @return true if the address is IPv4, false otherwise
      */
     public boolean isIpv4() {
-        return isIpv4;
+        return ipv == 4;
     }
     
     /**
@@ -66,7 +81,7 @@ public class Address extends Host {
      * @return true if the address is IPv6, false otherwise
      */
     public boolean isIpv6() {
-        return isIpv6;
+        return ipv == 6;
     }
     
     /**
@@ -102,6 +117,32 @@ public class Address extends Host {
             return false;
         }
     }
+    
+    /**
+     * Parses the hexadecimal representation of an IPv4 address.
+     * 
+     * @param name
+     *            the host name representing an IPv4 address
+     * @return the hexadecimal representation
+     * @throws MalformedAddress
+     *             when an IPv4 address cannot be parsed by the given host name
+     */
+    public static String parseIpv4(String name) throws MalformedAddress {
+        return new Address(name).parseIpv4().hex;
+    }
+    
+    /**
+     * Parses the hexadecimal representation of an IPv6 address.
+     * 
+     * @param name
+     *            the host name representing an IPv6 address
+     * @return the hexadecimal representation
+     * @throws MalformedAddress
+     *             when an IPv6 address cannot be parsed by the given host name
+     */
+    public static String parseIpv6(String name) throws MalformedAddress {
+         return new Address(name).parseIpv6().hex;
+    }
 
     /**
      * Tells whether a host name is an address or not.
@@ -117,16 +158,17 @@ public class Address extends Host {
     
     private void parse() throws MalformedAddress {
         String address = getName();
-        if (0 < getName().indexOf(".")) {
-            hex = parseIpv4(address);
-        } else if (0 < getName().indexOf(":")) {
-            parseIpv6(address);
+        if (-1 < getName().indexOf(".")) {
+            parseIpv4();
+        } else if (-1 < getName().indexOf(":")) {
+            parseIpv6();
         } else {
             throw new MalformedAddress(address, "Not an address!");
         }
     }
 
-    private static String parseIpv4(String address) throws MalformedAddress {
+    private Address parseIpv4() throws MalformedAddress {
+        String address = getName();
         StringBuilder hex = new StringBuilder(); 
         int mask = 0;
         int subnet = 'a';
@@ -168,11 +210,90 @@ public class Address extends Host {
             }
         }
         hex.append(String.format("%02x", mask));
-        return hex.toString();
+        this.hex = hex.toString();
+        ipv = 4;
+        return this;
     }
     
-    public static void main(String[] args) {
-        String[] ipv4s = new String[] {
+    private Address parseIpv6() throws MalformedAddress {
+        String address = getName();
+        StringBuilder suffhex = new StringBuilder(); 
+        StringBuilder prefhex = new StringBuilder(); 
+        StringBuilder comment = new StringBuilder(); 
+        int mask = 0;
+        char d = 0;
+        boolean isPadded = false;
+        boolean isComment = false;
+        for (char c : getName().toCharArray()) {   
+            if (isComment) {
+                comment.append(c);
+            } else {
+                switch (c) {
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                case 'a':
+                case 'b':
+                case 'c':
+                case 'd':
+                case 'e':
+                case 'f':
+                    mask *= 16;
+                    mask += Integer.parseInt("" + c, 16);
+                    break;
+                case ':':
+                    if (d == ':') {
+                        if (isPadded) 
+                            throw new MalformedAddress(address, "IPv6 adress can only be zero-padded at one point!");
+                        isPadded = true;
+                    } else {
+                        if (isPadded) {
+                            suffhex.append(String.format("%04x", mask));                    
+                        } else {
+                            prefhex.append(String.format("%04x", mask));                                        
+                        }                    
+                    }
+                    mask = 0;
+                    break;
+                case '%':
+                    isComment = true;
+                    break;
+                default:
+                    throw new MalformedAddress(address, "Illegal characters for an IPv4 address!");
+                }
+                d = c; // step!
+            }
+        }            
+        if (isPadded) {
+            suffhex.append(String.format("%04x", mask));                    
+        } else {
+            prefhex.append(String.format("%04x", mask));                                        
+        }
+        int padding = 32 - (prefhex.length() + suffhex.length());
+        if (padding < 0)
+            throw  new MalformedAddress(address, "Too large data for an IPv6 adress!");
+        hex = String.format("%s%0" + padding  + "x%s", prefhex, 0, suffhex);
+        if (0 < comment.length()) {
+            this.comment = comment.toString();            
+        }
+        ipv = 6;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "{ hex => " + hex + ", ipv => " + ipv + ", comment => " + comment + " }";
+    }
+    
+    public static void main(String[] args) { // TODO: create unit tests instead
+        String[] names = new String[] {
                 "127.0.0.1",
                 "213.66.58.72",
                 ".127.0.0.1",
@@ -182,24 +303,17 @@ public class Address extends Host {
                 "127.0.0.0.1",
                 "127.256.0.1",
                 "0.127.0.1",
+                "::1",
+                "fe80::1%lo0",
+                "2605:2700:0:3::4713:93e3",
         };
-        for (String ipv4 : ipv4s) {
+        for (String name : names) {
             try {
-                System.out.println(ipv4 + "\t: " + parseIpv4(ipv4));
+                System.out.println(new Address(name));
             } catch (MalformedAddress e) {
                 System.out.println(e.getAddress() + "\t: " + e.getMessage());
             }
         }
-    }
-    
-    private static String parseIpv6(String address) throws MalformedAddress {
-        throw new MalformedAddress(address, "Not implemented!"); // XXX: implement        
-    }
-
-    @Override
-    public String toString() {
-        return "{ hex => " + hex + ", isIpv4 => " + isIpv4 + ", isIpv6 => "
-                + isIpv6 + " }";
     }
     
 }
